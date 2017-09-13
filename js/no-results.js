98f5908bea4f7fc95b817dfcbb035a8e77f2c011
Sarah Johnston <johnston.sarah@gmail.com>
2017-09-13 16:26:26 -0500

    Update no-results.js
    Added check to remove PCI search if query was already expanded to entire index (pcAvailability = true).  Updated component template to comply with ES5.

--- js/no-results.js
+++ js/no-results.js
@@ -1,42 +1,16 @@
 // Enhance No Results tile
+app.controller('prmNoSearchResultAfterController', [function () {
+  var vm = this;
+  vm.getSearchTerm = getSearchTerm;
+  vm.pciSetting = vm.parentCtrl.searchStateService.searchObject.pcAvailability || '';
+  function getSearchTerm() {
+    return vm.parentCtrl.term;
+  }
+}]);
 
-	app.controller('prmNoSearchResultAfterController', [function () {
-		var vm = this;
-		vm.getSearchTerm = getSearchTerm;
-		function getSearchTerm() {
- 			return vm.parentCtrl.term;
- 		}
-	}]);
-
-	app.component('prmNoSearchResultAfter',{
-		bindings: {parentCtrl: '<'},
-		controller: 'prmNoSearchResultAfterController',
-		template: `<md-card class="default-card zero-margin _md md-primoExplore-theme">
-					<md-card-title>
-						<md-card-title-text>
-							<span translate="" class="md-headline ng-scope">No records found</span>
-						</md-card-title-text>
-					</md-card-title>
-					<md-card-content>
-					
-						
-						<p>
-						<span>There are no results matching your search:
-						<blockquote><i>{{$ctrl.getSearchTerm()}}</i>.</blockquote>
-						<!-- Update to your domain and view code -->
-						<a href="https://[primo-domain]/primo-explore/search?query=any,contains,{{$ctrl.getSearchTerm()}}&tab=default_tab&search_scope=Everything&vid=[vid_code]&offset=0&sortby=rank&pcAvailability=true"><b>Try again searching items held at other libraries?</b></a>
-						</span>
-						</p>
-						<p><span translate="" class="bold-text ng-scope">Suggestions:</span></p>
-						<ul><li translate="" class="ng-scope">Make sure that all words are spelled correctly.</li>
-						<li translate="" class="ng-scope">Try a different search scope.</li>
-						<li translate="" class="ng-scope">Try different search terms.</li>
-						<li translate="" class="ng-scope">Try more general search terms.</li>
-						<li translate="" class="ng-scope">Try fewer search terms.</li>
-						</ul>
-						<!-- Other helpful links -->
-						<p><b><a href="">Search WorldCat</a></b></p>
-						<p><b><a href="">Contact a Research Librarian for Assistance</a></b></p>
-					</md-card-content>
-				   </md-card>`
-	});
+// Update links in template line below to direct to your Primo server and WorldCat, etc.
+app.component('prmNoSearchResultAfter',{
+  bindings: {parentCtrl: '<'},
+  controller: 'prmNoSearchResultAfterController',
+  template: '<md-card class="default-card zero-margin _md md-primoExplore-theme"><md-card-title><md-card-title-text><span translate="" class="md-headline ng-scope">No records found</span></md-card-title-text></md-card-title><md-card-content><p><span>There are no results matching your search:<blockquote><i>{{$ctrl.getSearchTerm()}}</i>.</blockquote><div ng-if="$ctrl.pciSetting !== \'true\'"><a href="https://stolaf-primo.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,{{$ctrl.getSearchTerm()}}&tab=default_tab&search_scope=Everything&vid=01BRC_SOC&offset=0&sortby=rank&pcAvailability=true"><b>Try again searching items held at other libraries?</b></a></div></span></p><p><span translate="" class="bold-text ng-scope">Suggestions:</span></p><ul><li translate="" class="ng-scope">Make sure that all words are spelled correctly.</li><li translate="" class="ng-scope">Try a different search scope.</li><li translate="" class="ng-scope">Try different search terms.</li><li translate="" class="ng-scope">Try more general search terms.</li><li translate="" class="ng-scope">Try fewer search terms.</li></ul><p><b><a href="https://stolaf.on.worldcat.org/search?queryString=kw:{{$ctrl.getSearchTerm()}}&databaseList=283">Search WorldCat</a></b></p><p><b><a href="https://www.stolaf.edu/library/research/students.cfm">Contact a Research Librarian for Assistance</a></b></p></md-card-content></md-card>'
+});
